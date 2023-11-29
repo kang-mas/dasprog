deea695446bda77c355ab5051fd5e8f693e7393d
kang-mas <kang-mas@users.noreply.github.com>
2023-11-29 10:00:37 +0700

    Update quizfungsi.py
    update laagii

--- quizfungsi.py
+++ quizfungsi.py
@@ -2,9 +2,16 @@
 import os
 import json
 
+#fungssi untuk membersihkan layar
+def clear_screen():
+    if os.name=='posix':
+        os.system("clear")
+    if os.name=='nt':
+        os.system("cls")
+
 #deklarasi fungsi menu()
 def menu():
-    os.system("cls")
+    clear_screen()
     
     print("==Program Data Mahasiswa==")
     print("==========================")
@@ -17,13 +24,25 @@ def menu():
 
     ops = input("masukkan pilihan :")
     return ops
-    
+
+#deklarasi fungsi untuk menmpilkan data mahasiswa
+def menampilkan_data_mahasiswa(mahasiswa):
+    clear_screen()
+
+    print("====== Data Mahasiswa =======")
+    print(f"NIM     :{mahasiswa['nim']}")
+    print(f"Nama    :{mahasiswa['nama']}")
+    print(f"Umur    :{mahasiswa['umur']}")
+    print("=============================")
+
 #deklarasi fungsi cari_maahasiswa()
 def cari_mahasiswa(listmahasiswa,nim):
     if nim in listmahasiswa:
         mahasiswa=listmahasiswa[nim]
+        menampilkan_data_mahasiswa(mahasiswa)
         return mahasiswa
     else:
+        clear_screen()
         print("NIM tidak ditemukan!")
         return False
     
@@ -42,6 +61,8 @@ def tambah_mahasiswa(listmahasiswa):
     listmahasiswa.update({mahasiswa["nim"]:mahasiswa})
     with open('data.json', 'w') as f:
         json.dump(listmahasiswa,f)
+
+    menampilkan_data_mahasiswa(mahasiswa)
     return listmahasiswa
 
 
@@ -55,7 +76,7 @@ def hapus_mahasiswa(listmahasiswa):
 
 def ubah_mahasiswa(listmahasiswa):
     nim=input("Masukkan NIM data mahasiswa yang akan diubah :")
-    mahasiswa = cari_mahasiswa(listmahasiswa, nim):
+    mahasiswa = cari_mahasiswa(listmahasiswa, nim)
     if mahasiswa :
         nama=input("Masukkan Nama Mahasiswa   :")
         umur=input("Masukkan Umur Mahasiswa   :")
@@ -68,44 +89,55 @@ def ubah_mahasiswa(listmahasiswa):
         with open('data.json', 'w') as f:
             json.dump(listmahasiswa,f)
 
+        menampilkan_data_mahasiswa(mahasiswa)
         return listmahasiswa
     
     else:
+        clear_screen()
         print("NIM tidak ditemukan!")
         return False
     
+def main():    
+    listmahasiswa={}
+    if os.path.exists('data.json') :
+        f = open('data.json','r')
+        listmahasiswa.update(json.load(f))
+        print(listmahasiswa)
 
-listmahasiswa={}
-if os.path.exists('data.json') :
-    f = open('data.json','r')
-    listmahasiswa.update(json.load(f))
-    print(listmahasiswa)
-
-while True:
-    pilihan = menu()
-    
-    if pilihan == "1":
-        # tambah data dictionary
-        # passs
-        listmahasiswa=tambah_mahasiswa()
+    while True:
+        pilihan = menu()
         
-    elif pilihan=="2":
-        # cari data berdasrkan nim
-        #pass
-        nim=input("Masukkan NIM yang dicari [tidak boleh kosong] :")
-      
+        if pilihan == "1":
+            # tambah data dictionary
+            # passs
+            listmahasiswa=tambah_mahasiswa(listmahasiswa)
+            
+        elif pilihan=="2":
+            # cari data berdasrkan nim
+            #pass
+            nim=input("Masukkan NIM yang dicari [tidak boleh kosong] :")
+          
 
 
-    elif pilihan == "3":
-        # ubah data berdasarkan nim
-        #pass
-        
+        elif pilihan == "3":
+            # ubah data berdasarkan nim
+            #pass
+            ubah_mahasiswa(listmahasiswa)
+            
+
+        elif pilihan=="4":
+            #hhapus data maahasiswa
+            hapus_mahasiswa(listmahasiswa)
+            
+        elif pilihan=="5":
+            break
+        else :
+            print("pilihan tidak tersedia")
+
         while True:
-            x_quit=input("")
-            if x_quit =="":
-                break
+                x_quit=input("Tekan Enter")
+                if x_quit =="":
+                    break
 
-    elif pilihan=="4":
-        break
-    else :
-        print("pilihan tidak tersedia")
+if __name__=='__main__':
+    main()
