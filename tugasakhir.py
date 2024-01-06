f654f5ff64e513874875b49efed012507e4e8c6f
kang-mas <kang-mas@users.noreply.github.com>
2024-01-07 03:51:01 +0700

    Update

---
+++ tugasakhir.py
@@ -0,0 +1,16 @@
+import os
+class Bola:
+  def __init__(self, r):
+    self.r=r
+  
+  def volume_bola(self):
+    vol =
+
+    return vol
+  def 
+
+def main():
+  pass
+
+if __name__=="main__":
+  main()
