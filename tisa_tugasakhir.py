1beba9ae1a603e19d4b8e83a27bbe95edbcb19c2
kang-mas <kang-mas@users.noreply.github.com>
2024-01-20 12:11:29 +0700

    Update tisa_tugasakhir.py
    solusi

--- tisa_tugasakhir.py
+++ tisa_tugasakhir.py
@@ -2,7 +2,7 @@ import os
 import math
 
 class Bola:
-    def init(self, r):
+    def __init__(self, r): # edit double garis bawah
         self.r = r
 
     def volume_bola(self):
@@ -17,85 +17,86 @@ class Bola:
         volume = (2/3) * math.pi * self.r ** 3
         return volume
 
-    def clear_screen():
-        if os.name=='posix':
-           os.system("clear")
-        if os.name=='nt':
-           os.system("cls")
+## fungsi ini terpisah dari class Bola
+def clear_screen():
+    if os.name=='posix':
+       os.system("clear")
+    if os.name=='nt':
+       os.system("cls")
 
-    def menu():
-       clear_screen()
-	   
-         print ("==Program Menhitung Bola==")
-         print ("========================")
-         print ("2. Ubah Diameter Bola")
-         print ("3. Tampilkan Jari-Jari Bola")
-         print ("4. Tampilkan Volume Bola")
-         print ("5. Tampilkan Luas Kulit Bola")
-         print ("6. Tampilkan Volume Setengah Bola")
-		 print ("7. Tampilkan Semua")
-         print ("8. Keluar")
-         print ("========================")
-         
-         ops = input("Masukkan pilihan: ")
-		 return ops
-       
-    def main():
-        diameter = int(input("Masukkan Diameter Bola: "))
-        bola = Bola(diameter / 2)
+def menu():
+    clear_screen() # edit indent
+   
+    print ("==Program Menhitung Bola==") 
+    print ("========================")
+    print ("2. Ubah Diameter Bola")
+    print ("3. Tampilkan Jari-Jari Bola")
+    print ("4. Tampilkan Volume Bola")
+    print ("5. Tampilkan Luas Kulit Bola")
+    print ("6. Tampilkan Volume Setengah Bola")
+    print ("7. Tampilkan Semua")
+    print ("8. Keluar")
+    print ("========================")
+     
+    ops = input("Masukkan pilihan: ")
+    return ops
+   
+def main():
+    diameter = float(input("Masukkan Diameter Bola: ")) # edit # to float
+    bola = Bola(diameter / 2)
 
     while True:
         pilihan = menu ()
-		
+        
         if pilihan=="1":
-		  pass
-		
-        elif pilihan=="2":		
+          pass
+        
+        elif pilihan=="2":      
             diameter = int(input("Masukkan Diameter Bola: "))
             bola.r = diameter / 2
-			
+            
         elif pilihan=="3": #Tampilkan jari-jari
             print("==Program Menhitung Bola==")
             print("========================")
-			print(f"jari-jari bola : {bola.r}")
-			print("========================")
-			
+            print(f"jari-jari bola : {bola.r}")
+            print("========================")
+            
         elif pilihan=="4": # Tampilkan volume bola
-		    print("==Program Menhitung Bola==")
+            print("==Program Menhitung Bola==")
             print("========================")
             print(f"Volume Bola: {bola.volume_bola()}")
             print("========================")
-			
+            
         elif pilihan=="5": #Tampilkan luas kulit bola
-		    print("==Program Menhitung Bola==")
+            print("==Program Menhitung Bola==")
             print("========================")
             print(f"Luas Kulit Bola: {bola.luas_kulit_bola()}")
             print("========================")
-			
+            
         elif pilihan=="6": #Tampilkan volume setengah bola
-		    print("==Program Menhitung Bola==")
+            print("==Program Menhitung Bola==")
             print("========================")
             print(f"Volume Setengah Bola: {bola.volume_setengah_bola()}")
             print("========================")
-		
+        
         elif pilihan=="7": #Tampilkan jari - jari, volume, dan luas kulit bola
             print("==Program Menhitung Bola==")
             print("========================")
-			print(f"jari-jari bola : {bola.r}")
+            print(f"jari-jari bola : {bola.r}")
             print(f"Volume Bola: {bola.volume_bola()}")
-		    print(f"Luas Kulit Bola: {bola.luas_kulit_bola()}")
+            print(f"Luas Kulit Bola: {bola.luas_kulit_bola()}")
             print("========================")
-			
+            
         elif pilihan=="8":
             break
-			
+            
         else:
             print("Pilihan tidak tersedia")
 
-		while True:
+        while True:
             x_quit=input("")
             if x_quit =="":
                 break
-		
-if _name_ == "_main_":
-		main()
+        
+if __name__ == "__main__": ##edit double gaaris bawah
+        main()
